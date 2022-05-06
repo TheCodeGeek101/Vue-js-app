dc54ad45e095eaeee237885efee1cc03f462f994
TheCodeGeek101 <tizifathemba@gmail.com>
2022-05-06 19:28:08 +0200

    first commit

---
+++ src/components/Article.vue
@@ -0,0 +1,29 @@
+	<template>
+		<div>
+			<h2>Article component</h2>
+			<h2 v-bind="$attrs">{{title}}</h2>
+			<h2> likes - {{likes}}</h2>
+			<h2> Published - {{ isPublished ? 'Yes' : 'No' }}</h2>
+		</div>	
+	</template>
+
+	<script>
+		export default {
+			name: "Article",
+			props:{
+				title: {
+					type: String,
+					required: true,
+					default: "Article default title",
+				},
+				likes:Number,
+				isPublished:Boolean,
+			},
+		}
+
+	</script>
+
+	<style scope>
+	
+	
+	</style>
---
+++ src/components/Greet.vue
@@ -0,0 +1,16 @@
+<template>
+	<h1>Hello {{name}}</h1>
+</template>
+
+<script>
+	export default {
+		name: "Greet",
+		props: ['name']
+	}
+
+</script>
+
+<style scoped>
+
+
+</style>
